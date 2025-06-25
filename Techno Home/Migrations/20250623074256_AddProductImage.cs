abd896dfb0a794849b05df1f3d65e206c640909c
Avalon <rauankairav@gmail.com>
2025-06-25 17:19:27 +0800

    Add and facilitate EF migration with the database

---
+++ Techno Home/Migrations/20250623074256_AddProductImage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Techno_Home.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageFileName",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageFileName",
+                table: "Product");
+        }
+    }
+}

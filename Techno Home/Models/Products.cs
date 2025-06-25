9dca12efc1062526c0646e1df4aac818c969aa8d
Avalon <rauankairav@gmail.com>
2025-06-25 17:21:53 +0800

    Update up to the current product's details

--- Techno Home/Models/Products.cs
+++ Techno Home/Models/Products.cs
@@ -1,6 +1,18 @@
-﻿namespace Techno_Home.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Products
+namespace Techno_Home.Models
 {
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        // [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+        public string? ImageFileName { get; set; }
+    }
+    
     
 }

a5c9e1d1c05b0494a312ecba3e6f759d1f3b30e9
Rauan Zholaman <rauankairav@gmail.com>
2025-07-28 15:54:08 +0800

    Add product filerting class

---
+++ Techno Home/Models/ProductFilter.cs
@@ -0,0 +1,18 @@
+﻿using Techno_Home.Models;
+
+namespace Techno_Home.Models;
+
+public class ProductFilter
+{
+    public List<Product> Products { get; set; }
+    public List<Category> Categories { get; set; }
+    //public List<Product> Products { get; set; }
+    
+    public List<string>? SelectedTypes { get; set; } // For Category
+    public decimal? minPrice { get; set; }
+    public decimal? maxPrice { get; set; }
+    public List<string> SelectedBrands { get; set; }
+    
+    public List<string> Alltypes { get; set; }
+    public List<string> AllBrands { get; set; }
+}

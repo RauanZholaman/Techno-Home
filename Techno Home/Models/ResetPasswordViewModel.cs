e82ce5b434121aa7ede8b0c20b08cc5fd8302353
Rauan Zholaman <rauankairav@gmail.com>
2025-07-31 13:42:46 +0800

    Implement authorization and authenication feature

---
+++ Techno Home/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Techno_Home.Models;
+
+public class ResetPasswordViewModel
+{
+    [Required]
+    public string UserName { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string NewPassword { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
